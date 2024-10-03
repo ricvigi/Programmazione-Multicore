bad6407dcdfa8c64fcf8e5dc5aa14273bcb8eda5
ricvigi <ricvigi@proton.me>
2024-10-03 12:49:19 +0200

    Modified Makefile to only produce .o objects (no extension files are forbidden for clariry). Added histogram.h file for histogram_parallel.c exercise

---
+++ programs/mpi/histogram.h
@@ -0,0 +1,17 @@
+#ifndef HISTOGRAM_PARALLEL_H
+#define HISTOGRAM_PARALLEL_H
+void hist_parallel(float *data,
+		   int *data_count,
+		   float *min_meas,
+		   float *max_meas,
+		   int *bin_count);
+#endif
+#ifndef RANDOM_FLOAT_H
+#define RANDOM_FLOAT_H
+int random_float(int *nElements,
+		  const int *datacount,
+		  float **array,
+		  const float *max);
+#endif
+
+
---
+++ programs/mpi/histogram_parallel.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <mpi.h>
+#include <time.h>
+#include "histogram.h"
+
+// datacount stores how many elements are present in the array
+const int datacount = 50;
+
+// max value
+const float max = 5.0;
+int main(void) {
+  // Initialize the array
+  float *array = NULL;
+  
+  int nElements = 0;
+  random_float(&nElements, &datacount, &array, &max);
+  for (int i = 0; i < nElements; i++) {
+    printf("%f ", array[i]);
+  }
+
+  // remember to deallocate memory for the variable array.
+  free(array);
+  return 0;
+}
+
+/* Dinamically populates an array (array) of floats with
+   datacount elements. Elements of the array have a random
+   value 0 <= x <= max */
+int random_float(int *nElements,
+		  const int *datacount,
+		  float** array,
+		  const float *max) {
+  // initialize the seed for random number generation
+  srand((unsigned int)time(NULL));
+
+  
+  for (int i=0;i<*datacount;i++) {
+    // BE CAREFUL to use parenthesis in order to dereference
+    // the pointer BEFORE upgrading it. ++ incremental has
+    // priority over dereference!
+    (*nElements)++;
+    
+    // the following expression returns a float 0 <= x <= max
+    float t = ((float)rand()/(float)(RAND_MAX)) * *max;
+    
+    // dinamically allocate memory to the array
+    *array = realloc(*array, sizeof(float)* *nElements);
+    
+    // check that allocation was succesful, otherwise return 1
+    if (*array == NULL) {
+      printf("[*]ERROR Memory allocation failed!\n");
+      exit(1);
+    }
+    
+    // populate the array with element t
+    (*array)[*nElements - 1] = t;
+  }
+  return 0;
+}
