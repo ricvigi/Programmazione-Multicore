ab2c543ca0ae2a87abe89e1e3692d1b39f690f48
ricvigi <ricvigi@proton.me>
2024-10-12 18:27:39 +0200

    adjusted approx_pi exercise in programs/esercizimpi/main.c

--- programs/esercizimpi/main.c
+++ programs/esercizimpi/main.c
@@ -1,7 +1,8 @@
+/* This is a possibly parallel program that computes
+ * an approximation of pi*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <mpi.h>
-
 
 /* nummber_in_cicle = 0
  * for (toss = 0; toss < number_of_tosses; toss++) {
@@ -17,17 +18,18 @@ double my_rand() {
   return ((double)rand() / RAND_MAX)*2.0 - 1.0;
 }
 
-
-int main(int argc, char **argv) {
-  int size, rank, num_of_tosses, num_in_circle;
+void approx_pi(int seed, int ntosses) {
+  int size, rank, num_in_circle;
   int i;
   num_in_circle = 0;
-  srand(atoi(argv[1]));
-  num_of_tosses = atoi(argv[2]);
+  /* set the seed value for random number generation */
+  srand(seed);
+  
   MPI_Init(NULL,NULL);
+  double time = MPI_Wtime();
   MPI_Comm_size(MPI_COMM_WORLD,&size);
   MPI_Comm_rank(MPI_COMM_WORLD,&rank);
-  int num_local_tosses = num_of_tosses / size;
+  int num_local_tosses = ntosses / size;
   int local_num_in_circle = 0;
   
   for (i = 0; i < num_local_tosses; i++) {
@@ -40,9 +42,14 @@ int main(int argc, char **argv) {
   MPI_Reduce(&local_num_in_circle, &num_in_circle, 1,
 	     MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
   if (rank == 0) {
-    double pi = ((double)num_in_circle * 4)/num_of_tosses;
-    printf("%f\n", pi);
+    double pi = ((double)num_in_circle * 4)/ntosses;
+    double now = MPI_Wtime();
+    printf("%f %f\n", pi, now - time);
   }
   MPI_Finalize();
+}
+
+int main(int argc, char **argv) {
+  approx_pi(atoi(argv[1]), atoi(argv[2]));
   return 0;
 }
